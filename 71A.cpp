d6e2d24eec83fdb9fd43eeee0ff6d668b12c17ef
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-06-30 20:04:26 +0530

    Codeforces - 71A

---
+++ 71A.cpp
@@ -0,0 +1,23 @@
+// A. Way Too Long Words
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    while (n--){
+        string s;
+        cin >> s;
+
+        if (s.length() > 10){
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << '\n';
+        }else{
+            cout << s << '\n';
+        }
+    }
+    return 0;
+}
